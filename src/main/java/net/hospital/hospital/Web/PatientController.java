fa9fec855e473b7f05fad2c93c41fa23ac71d955
khadija <koutalib@gmail.com>
2024-03-31 16:12:13 +0000

    TP3

--- src/main/java/net/hospital/hospital/Web/PatientController.java
+++ src/main/java/net/hospital/hospital/Web/PatientController.java
@@ -1,4 +1,6 @@
 package net.hospital.hospital.Web;
+
+import jakarta.validation.Valid;
 import net.hospital.hospital.entities.Patient;
 import net.hospital.hospital.repositories.PatientRepository;
 import net.hospital.hospital.service.IHospitalService;
@@ -7,26 +9,25 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.List;
-
-@Controller // return data : HTML, JSON, XML,
+@Controller // Returns data: HTML, JSON, XML
 public class PatientController {
     @Autowired
     private PatientRepository patientRepository;
+
     @Autowired
     private IHospitalService iHospitalService;
-    private  PatientRestController restController;
-
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model ,@RequestParam(name ="page",defaultValue ="0")int page ,
-                                     @RequestParam(name ="size",defaultValue ="4")int size,
-                                     @RequestParam(name = "keyword" ,defaultValue = "")String keyword
-                        )
+                        @RequestParam(name ="size",defaultValue ="4")int size,
+                        @RequestParam(name = "keyword" ,defaultValue = "")String keyword
+    )
     {
 
         Page<Patient> ListPatient =patientRepository.findByNomContainsIgnoreCase(keyword,PageRequest.of(page,size));
@@ -36,22 +37,34 @@ public class PatientController {
         model.addAttribute("keyword",keyword);
         return "patients";
     }
-    @GetMapping("/Updatepatient")
+    @GetMapping("/admin/DeletePatient")
     @ResponseBody
-    public String  UpdatePatient (@RequestParam String nom, @RequestParam String tel){
-        iHospitalService.updateTelPatient(nom, tel);
-        return "redirect:/index";
-    }
- @GetMapping("/DeletePatient")
-    @ResponseBody
-    public String  DeletePatient(@RequestParam String nom){
-
-       iHospitalService.deletePatient(nom);
-
-        return "redirect:/index";
-
+    public RedirectView deletePatient(@RequestParam Long id, String keyword, int page) {
+        iHospitalService.deleteById(id);
+        return new RedirectView("/user/index?page=" + page + "&keyword=" + keyword);
     }
 
+    @GetMapping("/admin/FormPatient")
+    public String formPatients(Model model) {
+        model.addAttribute("patient", new Patient());
+        return "formPatients";
+    }
 
+    @PostMapping("/admin/save")
+    public RedirectView save(Model model, @Valid Patient patient, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new RedirectView("/user/FormPatient");
+        }
+        patientRepository.save(patient);
+        return new RedirectView("/user/index");
+    }
+    @GetMapping("/admin/UpdatePatient")
+    public String UpdatePatient(Model model ,Long id) {
+        Patient patient = patientRepository.findById(id).orElse(null);
+        if(patient ==null)throw new RuntimeException("Patienr introvable");
+
+        model.addAttribute("patient", patient);
+        return "updatePatient";
+    }
 
 }
--- src/main/java/net/hospital/hospital/entities/Patient.java
+++ src/main/java/net/hospital/hospital/entities/Patient.java
@@ -1,10 +1,11 @@
 package net.hospital.hospital.entities;
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import org.springframework.format.annotation.DateTimeFormat;
 import java.util.Collection;
 import java.util.Date;
 
@@ -15,9 +16,13 @@ public class Patient {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
      private Long id;
+    @NotEmpty
+    @Size(min =3 ,max =15)
      private String nom;
+    @Size(min=10,max=15)
      private String phone;
      @Temporal(TemporalType.DATE)
+     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private  Date dateNaissanec;
     private  boolean  malade ;
 
